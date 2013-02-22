f93cbb388ac960bee994a868692a6155f0106b22
igorm <igorm@mup.rh>
2013-02-22 15:49:42 +0100

    Još nešto dodano

--- Test3/src/Test3Class.java
+++ Test3/src/Test3Class.java
@@ -7,7 +7,7 @@ public class Test3Class {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 
-		System.out.println("Ovo je jako dugački tekst");
+		System.out.println("Ovo je jako dugački tekst koji nešto radi");
 		
 	}
 
